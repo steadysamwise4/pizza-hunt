927573d8fd4861948b4097ff9218f30c64f3bf78
Sam Davenport <samueldavenport@att.net>
2022-01-18 10:00:44 -0600

    added comment model and associated it with pizza. completed front end functionality. connected routes. added comment controller methods. implemented and fixed bug in dateFormat.js

--- utils/dateFormat.js
+++ utils/dateFormat.js
@@ -81,8 +81,11 @@ module.exports = (
     hour = 12;
   }
 
-  const minutes = dateObj.getMinutes();
+  let minutes = dateObj.getMinutes();
 
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
   // set `am` or `pm`
   let periodOfDay;
 
